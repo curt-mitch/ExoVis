ba5340e66bc6cfa14e92537e491cb39c46b9818c
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-14 16:53:20 -0700

    moved rendering function to js/src/renderer.js

---
+++ js/src/renderer.js
@@ -0,0 +1,8 @@
+// Create a WebGLRenderer
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerWidth);
+
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0x000000, 1.0);
+renderer.clear();
