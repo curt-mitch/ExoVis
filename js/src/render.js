04719936d0514ade3efec9ea125148c7f97c802d
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-14 11:31:15 -0700

    index.html conflict resolved

--- js/src/render.js
+++
@@ -1,6 +0,0 @@
-function render() {
-
-      cameraCube.rotation.copy(camera.rotation); // ties skybox camera to regular camera
-      renderer.render(sceneCube, cameraCube)
-      renderer.render(scene, camera );
-    }
