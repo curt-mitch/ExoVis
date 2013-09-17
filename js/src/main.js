2ede7a4a428b92adab01027ef2702212c4f082d8
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-17 16:52:16 -0700

    added multiple node 'require' statements to min.'s for use with browserify.

---
+++ js/src/main.js
@@ -0,0 +1,6 @@
+var animate = require('./animate.js');
+var cameras = require('./main.js');
+var render = require('./render.js');
+var renderer = require('./renderer.js');
+var skybox = require('./skybox.js');
+var scene = require('./scene.js');
