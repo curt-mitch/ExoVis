4fbed39fb7eb1f72459ff80ea8b1df123a20ac52
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-18 12:53:58 -0700

    minor alignment and syntax corrections

--- js/src/render.js
+++ js/src/render.js
@@ -1,5 +1,5 @@
 function render() {
   cameraCube.rotation.copy(camera.rotation); // ties skybox camera to regular camera
-  renderer.render(sceneCube, cameraCube)
-  renderer.render(scene, camera );
+  renderer.render(sceneCube, cameraCube);
+  renderer.render(scene, camera);
 }
