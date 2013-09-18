817f86ec8a076a4cfd4366319a71dbf3ac06efec
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-18 13:53:23 -0700

    removed scene.js

--- js/src/scene.js
+++
@@ -1,7 +0,0 @@
-// SCENE
-var skybox = require('./skybox');
-module.exports = function(skybox){
-  var scene = new THREE.Scene();
-  var sceneCube = new THREE.Scene(); //this is a scene rendered specifically for the skybox
-  scene.add(skybox);
-};
