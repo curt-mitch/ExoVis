2b283a7a1e72c24ef9e890dfa76d004d13310c65
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-18 13:03:30 -0700

    fixed alignment of multiple sections

--- js/src/skybox.js
+++ js/src/skybox.js
@@ -1,28 +1,30 @@
 //add skybox
-    var urlPrefix = "textures/skybox/";
-    var urls = [
-      urlPrefix + "pos-x.png", urlPrefix + "neg-x.png",
-      urlPrefix + "pos-y.png", urlPrefix + "neg-y.png",
-      urlPrefix + "pos-z.png", urlPrefix + "neg-z.png"
-    ];
+var urlPrefix = "textures/skybox/";
+var urls = [
+  urlPrefix + "pos-x.png", urlPrefix + "neg-x.png",
+  urlPrefix + "pos-y.png", urlPrefix + "neg-y.png",
+  urlPrefix + "pos-z.png", urlPrefix + "neg-z.png"
+];
 
-    var cubemap = THREE.ImageUtils.loadTextureCube(urls); // load textures
-    cubemap.format = THREE.RGBFormat;
+var cubemap = THREE.ImageUtils.loadTextureCube(urls); // load textures
+cubemap.format = THREE.RGBFormat;
 
-    var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
-    shader.uniforms['tCube'].value = cubemap; // apply textures to shader
+var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
+shader.uniforms['tCube'].value = cubemap; // apply textures to shader
 
 // create shader material
-    var skyBoxMaterial = new THREE.ShaderMaterial( {
-      fragmentShader: shader.fragmentShader,
-      vertexShader: shader.vertexShader,
-      uniforms: shader.uniforms,
-      depthWrite: false,
-      side: THREE.BackSide
-    });
+var skyBoxMaterial = new THREE.ShaderMaterial( {
+  fragmentShader: shader.fragmentShader,
+  vertexShader: shader.vertexShader,
+  uniforms: shader.uniforms,
+  depthWrite: false,
+  side: THREE.BackSide
+});
 
 // create skybox mesh
-    var skybox = new THREE.Mesh(
-      new THREE.CubeGeometry(10000, 10000, 10000),
-      skyBoxMaterial
-    );
+var skybox = new THREE.Mesh(
+  new THREE.CubeGeometry(10000, 10000, 10000),
+  skyBoxMaterial
+);
+
+module.exports = skybox;
