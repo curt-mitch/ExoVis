7746f88a85328b121ec7cccbc6b99656fc0c851a
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-19 12:21:19 -0700

    added references to bodies.js and module export statement

--- js/src/animate.js
+++ js/src/animate.js
@@ -1,6 +1,8 @@
 var time = 0;
 var speed = 1;
-
+var bodies = require('./bodies');
+var render = require('./render');
+var controls = require('./controls');
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
@@ -9,6 +11,8 @@ function animate() {
   time = time + speed;
   var p1_angle = time * 0.01,
   p2_angle = time * 0.004;
-  planet1.position.set(200* Math.cos(p1_angle), 200*Math.sin(p1_angle), 0);
-  planet2.position.set(350* Math.cos(p2_angle), 350*Math.sin(p2_angle), 0);
+  bodies.planet1.position.set(200* Math.cos(p1_angle), 200*Math.sin(p1_angle), 0);
+  bodies.planet2.position.set(350* Math.cos(p2_angle), 350*Math.sin(p2_angle), 0);
 }
+
+module.exports = animate;
