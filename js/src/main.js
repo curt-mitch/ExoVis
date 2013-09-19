1a8b9a52962c478813f2824d4487434c5e31865b
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-19 12:37:51 -0700

    added require statements for remaining modules; added document ready function for calling animate

--- js/src/main.js
+++ js/src/main.js
@@ -1,6 +1,12 @@
-var animate = require('./animate.js');
-var cameras = require('./main.js');
+var cameras = require('./cameras.js');
 var render = require('./render.js');
 var renderer = require('./renderer.js');
 var skybox = require('./skybox.js');
-var scene = require('./scene.js');
+var scenes = require('./scenes.js');
+var controls = require('./controls.js');
+var bodies = require('./bodies.js');
+var animate = require('./animate.js');
+
+$(document).ready(function(){
+  animate();
+});
