fe79086de33c6298cbada6f1967e8608b281d7f8
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-19 12:25:28 -0700

    added module export line

--- js/src/cameras.js
+++ js/src/cameras.js
@@ -2,6 +2,8 @@
 // args sig -> new THREE.PerspectiveCamera( FOV, viewAspectRatio, zNear, zFar );
 var camera = new THREE.PerspectiveCamera(45, document.body.clientWidth / document.body.clientHeight, 1, 10000);
 camera.position.z = 1500;
+module.exports.camera = camera;
 
 // camera dedicated to skybox
 var cameraCube = new THREE.PerspectiveCamera(45, document.body.clientWidth / document.body.clientHeight, 1, 10000);
+module.exports.cameraCube = cameraCube;
