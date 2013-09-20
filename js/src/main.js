192e1ff8352c983d7d02261687c95ffcc7c14ef2
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-19 17:00:14 -0700

    added require statement for systemInfo.js

--- js/src/main.js
+++ js/src/main.js
@@ -5,6 +5,7 @@ var skybox = require('./skybox.js');
 var scenes = require('./scenes.js');
 var controls = require('./controls.js');
 var bodies = require('./bodies.js');
+var systemInfo = require('./systemInfo.js');
 var animate = require('./animate.js');
 
 $(document).ready(function(){
