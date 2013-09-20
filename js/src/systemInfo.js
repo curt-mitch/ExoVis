40731874af34d5f452e654abcfff5039bd8b8549
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-20 15:50:05 -0700

    moved h1 title element to index.html

--- js/src/systemInfo.js
+++ js/src/systemInfo.js
@@ -1,8 +1,3 @@
 var renderer = require('./renderer');
 
-var title = document.createElement('div');
-$(title).attr('id', 'title');
-title.innerHTML = '<h1>ExoVis - A Visualizer for Exoplanetary Systems</h1>';
-document.body.appendChild(title);
-
 module.exports = title;
