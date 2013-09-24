c3a05ff86481cc67a6965299a60a51f7ca36dc8f
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-24 11:17:36 -0700

    added comments on some schema values

--- server.js
+++ server.js
@@ -18,22 +18,16 @@ var exoplanetDataSchema = new mongoose.Schema({
   pl_discmethod: String, // planet discovery method
   pl_pnum: Number, // number of planets
   pl_orbper: Number, // planet orbit period (days)
-  pl_orbsmax: Number, // orbit period semimajor axis
+  pl_orbsmax: Number, // orbit period semimajor axis (AU)
   pl_orbeccen: Number, // orbit eccentricity
-  pl_orbincl: Number, // orbit inclination
+  pl_orbincl: Number, // orbit inclination (degrees)
   pl_massj: Number, // planet mass (Jupiter)
-  pl_radj: Number, // planet radius
+  pl_radj: Number, // planet radius (Jupiter)
   pl_dens: Number, // planet density (g/cm^3)
   pl_ttvflag: Number, // planet transit timing variation flag
   pl_kepflag: Number, // planet Kepler Field flag
   ra: Number, // right ascension (degrees)
   dec: Number, // declination (degrees)
-  pl_eqt: Number, // planet equilibrium temperature (K)
-  pl_masse: Number, // planet mass (Earth)
-  pl_rade: Number, // planet radius (Earth)
-  pl_disc: Number, // year of discovery
-  pl_status: Number, // planet status
-  pl_pelink: String, // link to Exoplanet Encyclopaedia
   st_dist: Number, // distance (parsecs)
   st_vj: Number, // V (Johnson) system magnitude
   st_teff: Number, // effective stellar temperature (K)
@@ -42,10 +36,15 @@ var exoplanetDataSchema = new mongoose.Schema({
   hd_name: String, // HD name
   hip_name: String, // HIP name
   st_spstr: String, // stellar spectral type
-  st_lum: Number // stellar luminosity (log solar luminosity)
+  st_lum: Number, // stellar luminosity (log solar luminosity)
+  pl_eqt: Number, // planet equilibrium temperature (K)
+  pl_masse: Number, // planet mass (Earth)
+  pl_rade: Number, // planet radius (Earth)
+  pl_disc: Number, // year of discovery
+  pl_status: Number // planet status
 });
 
-var System = mongoose.model('System', exoplanetDataSchema, 'NASAdata');
+var System = mongoose.model('System', exoplanetDataSchema, 'systemdata');
 
 var query = System.findOne({'pl_hostname': 'HD 196885'}, null, function (err, system) {
   if (err) return handleError(err);
