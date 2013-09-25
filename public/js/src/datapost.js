03425616097965d590c33f17ef9083292129f0e9
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-25 14:52:04 -0700

    new file for posting text data to page

---
+++ public/js/src/datapost.js
@@ -0,0 +1,48 @@
+var datapost = $(document).ready(function(){
+  var starname = $('#starname');
+  var starmass = $('#starmass');
+  var starsize = $('#starsize');
+  var startemp = $('#startemp');
+  var RA = $('#RA');
+  var dec = $('#dec');
+  var distance = $('#distance');
+  var planetnum = $('#planetnum');
+  var planetname = $('#planetname');
+  var orbitlength = $('#orbitlength');
+  var planettemp = $('#planettemp');
+  var planetmass = $('#planetmass');
+  var discoverymethod = $('#discoverymethod');
+  var discoveryyear = $('#discoveryyear');
+
+  var url = 'http://localhost:3000/systems/'+encodeURIComponent($("#starlist").val());
+  console.log(url);
+
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    success: function(data, status){
+      starname.append(data[0].pl_hostname);
+      starmass.append(data[0].st_mass);
+      starsize.append(data[0].st_rad);
+      startemp.append(data[0].st_teff);
+      RA.append(data[0].ra.toFixed(2) + "º");
+      dec.append(data[0].dec.toFixed(2) + "º");
+      distance.append((data[0].st_dist * 3.26).toFixed(2) + " light-years");
+      planetnum.append(data[0].pl_pnum);
+      planetname.append(data[0].pl_hostname + data[0].pl_letter);
+      orbitlength.append(data[0].pl_orbper.toFixed(2) + " days");
+      planettemp.append(data[0].pl_eqt || "unknown");
+      planetmass.append(data[0].pl_masse + " x Earth");
+      discoverymethod.append(data[0].pl_discmethod);
+      discoveryyear.append(data[0].pl_disc);
+
+
+    },
+    error: function(){
+      console.log('JSON not loaded successfully');
+    }
+  });
+});
+
+
+module.exports.datapost = datapost;
