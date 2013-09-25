d4a4069ec15133b3ca72df16103a47b6b145fc79
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-25 15:17:51 -0700

    updates to database querying

--- controllers/systems.js
+++ controllers/systems.js
@@ -3,6 +3,7 @@ var System = mongoose.model('System');
 
 // HD 196885
 module.exports.getSystem = function(req, res, next) {
+  // pass 2nd optional argument after the pl_hostname key-value pair to specify which parts of Mongo object you want returned (pl_hostname, etc.)
   System.find({'pl_hostname': req.params.star}, function (err, system) {
     if(err) {
       console.error('Error retrieving system: ', err);
