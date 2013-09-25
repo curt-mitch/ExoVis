218697d7d08e07c717fed4eb22779ffdc0c5e78b
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-25 16:16:32 -0700

    removed variable names for jquery objects in datapost.js; removed jquery-csv script reference from html file

--- public/bundle.js
+++ public/bundle.js
@@ -83,46 +83,28 @@ controls.addEventListener('change', render);
 module.exports = controls;
 },{"./cameras":3,"./render":7}],5:[function(require,module,exports){
 var datapost = $(document).ready(function(){
-  var starname = $('#starname');
-  var starmass = $('#starmass');
-  var starsize = $('#starsize');
-  var startemp = $('#startemp');
-  var RA = $('#RA');
-  var dec = $('#dec');
-  var distance = $('#distance');
-  var planetnum = $('#planetnum');
-  var planetname = $('#planetname');
-  var orbitlength = $('#orbitlength');
-  var distancefromstar = $('#distancefromstar');
-  var planettemp = $('#planettemp');
-  var planetmass = $('#planetmass');
-  var planetradius = $('#planetradius');
-  var discoverymethod = $('#discoverymethod');
-  var discoveryyear = $('#discoveryyear');
-
   var url = 'http://localhost:3000/systems/'+encodeURIComponent($("#starlist").val());
-  console.log(url);
 
   $.ajax({
     url: url,
     dataType: 'json',
     success: function(data, status){
-      starname.append(data[0].pl_hostname);
-      starmass.append(data[0].st_mass);
-      starsize.append(data[0].st_rad);
-      startemp.append(data[0].st_teff + "K");
-      RA.append(data[0].ra.toFixed(2) + "º");
-      dec.append(data[0].dec.toFixed(2) + "º");
-      distance.append((data[0].st_dist * 3.26).toFixed(2) + " light-years");
-      planetnum.append(data[0].pl_pnum);
-      planetname.append(data[0].pl_hostname + " " + data[0].pl_letter);
-      orbitlength.append(data[0].pl_orbper && data[0].pl_orbper.toFixed(2) + " days" || "N/A");
-      distancefromstar.append(data[0].pl_orbsmax && data[0].pl_orbsmax.toFixed(2) + " AU" || "N/A");
-      planettemp.append(data[0].pl_eqt && data[0].pl_eqt.toFixed(2) + "K" || "N/A");
-      planetmass.append(data[0].pl_masse && data[0].pl_masse.toFixed(0) || "N/A");
-      planetradius.append(data[0].pl_rade && data[0].pl_rade.toFixed(0) || "N/A");
-      discoverymethod.append(data[0].pl_discmethod);
-      discoveryyear.append(data[0].pl_disc);
+      $('#starname').append(data[0].pl_hostname);
+      $('#starmass').append(data[0].st_mass);
+      $('#starsize').append(data[0].st_rad);
+      $('#startemp').append(data[0].st_teff + "K");
+      $('#RA').append(data[0].ra.toFixed(2) + "º");
+      $('#dec').append(data[0].dec.toFixed(2) + "º");
+      $('#distance').append((data[0].st_dist * 3.26).toFixed(2) + " light-years");
+      $('#planetnum').append(data[0].pl_pnum);
+      $('#planetname').append(data[0].pl_hostname + " " + data[0].pl_letter);
+      $('#orbitlength').append(data[0].pl_orbper && data[0].pl_orbper.toFixed(2) + " days" || "N/A");
+      $('#distancefromstar').append(data[0].pl_orbsmax && data[0].pl_orbsmax.toFixed(2) + " AU" || "N/A");
+      $('#planettemp').append(data[0].pl_eqt && data[0].pl_eqt.toFixed(2) + "K" || "N/A");
+      $('#planetmass').append(data[0].pl_masse && data[0].pl_masse.toFixed(0) || "N/A");
+      $('#planetradius').append(data[0].pl_rade && data[0].pl_rade.toFixed(0) || "N/A");
+      $('#discoverymethod').append(data[0].pl_discmethod);
+      $('#discoveryyear').append(data[0].pl_disc);
 
     },
     error: function(){
