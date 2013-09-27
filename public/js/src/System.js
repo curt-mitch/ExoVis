8941309fc1df6e7735eb229e723936ffd7339e16
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-27 12:18:28 -0700

    added larger star spectrum-Hex coloring object

--- public/js/src/System.js
+++ public/js/src/System.js
@@ -1,27 +1,69 @@
 var Body = require('./Body.js');
 
 var starColors = {
-  'O5': 0x9DB4FF,
-  'B1': 0xA2B9FF,
-  'B3': 0xA7BCFF,
-  'B5': 0xAABFFF,
-  'B8': 0xAFC3FF,
-  'A1': 0xBACCFF,
-  'A3': 0xC0D1FF,
-  'A5': 0xCAD8FF,
-  'F0': 0xE4E8FF,
-  'F2': 0xEDEEFF,
-  'F5': 0xFBF8FF,
-  'F8': 0xFFF9F9,
-  'G2': 0xFFF5EC,
-  'G5': 0xFFF4E8,
-  'G8': 0xFFF1DF,
-  'K0': 0xFFEDB1,
-  'K4': 0xFFD7AE,
-  'K7': 0xFFC690,
-  'M2': 0xFFBE7F,
-  'M4': 0xFFBB7B,
-  'M6': 0xFFBB7B
+  'O5': 0x9bb0ff,
+  'O6': 0xa2b8ff,
+  'O7': 0x9db1ff,
+  'O8': 0x9db1ff,
+  'O9': 0x9ab2ff,
+  'B0': 0x9cb2ff,
+  'B1': 0xa0b6ff,
+  'B2': 0xa0b4ff,
+  'B3': 0xa5b9ff,
+  'B4': 0xa4b8ff,
+  'B5': 0xaabfff,
+  'B6': 0xacbdff,
+  'B7': 0xadbfff,
+  'B8': 0xb1c3ff,
+  'B9': 0xb5c6ff,
+  'A0': 0xb9c9ff,
+  'A1': 0xb5c7ff,
+  'A2': 0xbbcbff,
+  'A3': 0xbfcfff,
+  'A5': 0xcad7ff,
+  'A6': 0xc7d4ff,
+  'A7': 0xc8d5ff,
+  'A8': 0xd5deff,
+  'A9': 0xdbe0ff,
+  'F0': 0xe0e5ff,
+  'F2': 0xecefff,
+  'F4': 0xe0e2ff,
+  'F5': 0xf8f7ff,
+  'F6': 0xf4f1ff,
+  'F7': 0xf6f3ff,
+  'F8': 0xfff7fc,
+  'F9': 0xfff7fc,
+  'G0': 0xfff8fc,
+  'G1': 0xfff7f8,
+  'G2': 0xfff5f2,
+  'G4': 0xfff1e5,
+  'G5': 0xfff4ea,
+  'G6': 0xfff4eb,
+  'G7': 0xfff4eb,
+  'G8': 0xffedde,
+  'G9': 0xffefdd,
+  'K0': 0xffeedd,
+  'K1': 0xffe0bc,
+  'K2': 0xffe3c4,
+  'K3': 0xffdec3,
+  'K4': 0xffd8b5,
+  'K5': 0xffd2a1,
+  'K7': 0xffc78e,
+  'K8': 0xffd1ae,
+  'M0': 0xffc38b,
+  'M1': 0xffcc8e,
+  'M2': 0xffc483,
+  'M3': 0xffce81,
+  'M4': 0xffc97f,
+  'M5': 0xffcc6f,
+  'M6': 0xffc370,
+  'M8': 0xffc66d,
+  'A4': 0xc3d2ff,
+  'F3': 0xe3e6ff,
+  'G3': 0xffeee2,
+  'M7': 0xffa561,
+  'M9': 0xffe99a,
+  'N': 0xff9d00
 };
 
 var System = module.exports = function(options) {
@@ -48,7 +90,8 @@ System.prototype.init = function(data) {
   this.info.pl_orbper = data[0].pl_orbper; // planet orbital period (days)
 
   var starSpectrum = this.info.st_spstr.substr(0,2);
-  var starRadius = Math.floor(this.info.st_rad);
+  var starRadius = Math.floor(this.info.st_rad * 50);
+  console.log(star);
 
   this.ambient = new THREE.AmbientLight(0xffffff);
   this.scene.add(this.ambient);
@@ -61,7 +104,7 @@ System.prototype.init = function(data) {
   this.starlight = new THREE.PointLight(starColors[starSpectrum], 10, 1000);
   this.star.add(this.starlight);
 
-  // TODO: LOOP OVER ALL THE PLANETS AN ADD THEM
+  // TODO: LOOP OVER ALL THE PLANETS AND ADD THEM
   this.addPlanet(new Body({
     scene: this.scene
   }));
