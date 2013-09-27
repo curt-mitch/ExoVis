fd4b0d871dbbdc7c505402cd40d1ba106716a48f
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-27 10:59:50 -0700

    removed 'http://localhost:3000 from data post url

--- public/js/src/datapost.js
+++ public/js/src/datapost.js
@@ -1,5 +1,5 @@
 var datapost = function(){
-  var url = 'http://localhost:3000/systems/'+$("#starlist").val();
+  var url = '/systems/'+$("#starlist").val();
   // console.log(url);
   return $.ajax({
     url: url,
