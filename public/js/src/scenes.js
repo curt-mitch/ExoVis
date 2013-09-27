3793cafff933f31e8e8583bd6f413dd9f7e6ee6f
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2013-09-26 17:53:43 -0700

    multiple files deleted during reorganization

--- public/js/src/scenes.js
+++
@@ -1,8 +0,0 @@
-// SCENE
-var skybox = require('./skybox');
-var scene = new THREE.Scene();
-scene.add(skybox);
-module.exports.scene = scene;
-
-var sceneCube = new THREE.Scene(); //this is a scene rendered specifically for the skybox
-module.exports.sceneCube = sceneCube;
